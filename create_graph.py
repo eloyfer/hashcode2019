2bf8c607d6257b79b87e61f068742311455eb8fb
bgalb <gal.katzhendler@mail.huji.ac.il>
2019-02-28 23:12:59 +0200

    asd

--- create_graph.py
+++ create_graph.py
@@ -1,14 +1,16 @@
 import numpy as np
 import networkx as nx
-# import read_data
-def some_func(filename):
-    return 10*np.random.rand(80000, 100)
+#from read_data import *
+import read_data.py
 
-array = some_func("blala").astype('int')
+
+array, im_inds = read_data.read_data("a_example.txt", True)
 # array = read_data.read_data("a_example.txt")
-
+ones = np.ones_like(array)
 dot_prod = np.dot(array, array.T)
-dot_ones = np.dot(1, array.T)
+dot_ones = np.dot(ones, array.T)
+
+del ones
 
 score1 = dot_prod               #np.dot(array, array.T)
 score2 = dot_ones - dot_prod    #np.dot(1-array, array.T)
@@ -18,4 +20,18 @@ stack_the_layers = np.stack((score1, score2, score3), axis=-1)
 adj = np.min(stack_the_layers, axis=-1)
 
 G = nx.from_numpy_matrix(adj)
-G_directed = G.to_directed()
+G = nx.maximum_spanning_tree(G)
+G = G.to_directed()
+slideshow = list(nx.dfs_preorder_nodes(G, source=0))
+
+    # print(slideshow)
+with open(args.input + '.out', 'w') as fid:
+    fid.write(str(len(slideshow)) + '\n')
+    for elem in slideshow:
+        inds = im_inds[elem]
+        if type(inds) == int:
+            inds = [inds]
+        inds = list(inds)
+        inds = [str(x) for x in inds]
+        fid.write(' '.join(inds) + '\n')
+
