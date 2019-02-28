e8f18df9d5680679a2b0a9585e13f03fd7fa923b
Eran Nussinovitch <eran.nussinovitch@gmail.com>
2019-02-28 15:10:32 -0500

    working

--- simple_random.py
+++ simple_random.py
@@ -1,5 +1,6 @@
 import numpy as np
-
+from read_data import read_data
+from random import shuffle
 
 def get_score(array):
 	score1 = np.dot(array, array.T)
@@ -11,39 +12,67 @@ def get_score(array):
 	return adj
 
 if __name__ == '__main__':
-	# from read_data import read_data
-	# A = read_data('a_example.txt')
-	A = np.array([[1,1,1],[1,1,1],[1,1,1],[1,1,1]])
+
+	RANDOM_SAMPLES_PER_CHOICE = 10
+	# A = read_data('b_lovely_landscapes.txt')
+	# A = read_data('d_pet_pictures.txt')
+	A = read_data('c_memorable_moments.txt')
+
 	#calc scores
 	adj = get_score(A)
-	A = np.append(A, np.array([range(A.shape[0])]).T, axis=1)
 
-	(rows,cols) = A.shape #rows is number of images, cols is tags
+	(rows, cols) = A.shape #rows is number of images, cols is tags
+	indicies = list(range(rows))
+
+	# Get random first im
 	idx = np.random.randint(rows)
-	first_im =  A[idx, :]
-	A = np.delete(A, idx, 0)
+	indicies.remove(idx) # Removes from the rest
+	slideshow_list = [idx]
 
-	slideshow_list = [first_im[-1]]
+	while len(indicies) > 0:
 
-	rows = A.shape[0]
-	cur_im = first_im
-	while rows > 0: #While there are still images
+		# Get random 10 indicies
+		random_indicies = indicies.copy()
+		shuffle(random_indicies)
+		random_ten_idx = random_indicies[:RANDOM_SAMPLES_PER_CHOICE]
+		orig_row = adj[idx, :]
 
-		if rows > 10:
-			random_ten_idx = np.random.permutation(rows)[:10]
-			random_ten = A[random_ten_idx,:]
+		#get best index from
+		idx_score_row = adj[idx,random_ten_idx]
+		best_idx = np.argmax(idx_score_row)
+		chosen_idx = random_indicies[best_idx]
 
-			score = get_score(random_ten, cur_im)
-			if score < 1:
-				break #adding more slides won't add to show
+		# add chosen index to slideshow + remove from indices list
+		slideshow_list.append(chosen_idx)
+		indicies.remove(chosen_idx)
 
-			slideshow_list.append([])
-		else:
-			#just check them all
-			score = get_score(A, cur_im)
-			if score < 1:
-				break #adding more slides won't add to show
-			slideshow_list.append([])
-
-	# This is final list
 	print(slideshow_list)
+	print(len(slideshow_list))
+
+
+
+
+
+	#
+	# slideshow_list = [first_im[-1]]
+	#
+	# rows = A.shape[0]
+	# cur_im = first_im
+	# while rows > 0: #While there are still images
+	#
+	# 	if rows > 10:
+	# 		random_ten_idx = np.random.permutation(rows)[:10]
+	# 		random_ten = A[random_ten_idx,:]
+	#
+	# 		score = get_score(random_ten, cur_im)
+	# 		if score < 1:
+	# 			break #adding more slides won't add to show
+	#
+	# 		slideshow_list.append([])
+	# 	else:
+	# 		#get remaining indicies list
+	#
+	# 		slideshow_list.append([])
+	#
+	# # This is final list
+	# print(slideshow_list)
